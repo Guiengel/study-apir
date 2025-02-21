c0c1be7c8a57ef86ed61c4fcff1a6992f04adc2c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 22:47:33 -0300

    Adicionando ControllerProduto

---
+++ src/main/java/com/github/guiengel/study_apir/ControllerProduto.java
@@ -0,0 +1,34 @@
+package com.github.guiengel.study_apir;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+
+@RestController
+@RequestMapping("Produtos")
+public class ControllerProduto {
+    @PostMapping
+    public ResponseEntity<String> create(){
+        return ResponseEntity.status(201).body("Produto cadastrado");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update(){
+        return ResponseEntity.status(200).body("Produto atualizado");
+    }
+
+    @GetMapping
+    public ResponseEntity<String> find(){
+        return ResponseEntity.status(200).body("Maça");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(){
+        return ResponseEntity.status(204).build();
+    }
+}
