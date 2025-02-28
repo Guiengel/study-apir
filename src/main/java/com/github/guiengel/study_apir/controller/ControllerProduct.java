e6c60a8005b1db8680b4678275df0188791e4930
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 22:52:05 -0300

    feat: adding service

--- src/main/java/com/github/guiengel/study_apir/controller/ControllerProduct.java
+++ src/main/java/com/github/guiengel/study_apir/controller/ControllerProduct.java
@@ -1,5 +1,6 @@
 package com.github.guiengel.study_apir.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,26 +9,39 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.guiengel.study_apir.service.ProductService;
+
 @RestController
 @RequestMapping("Produtos")
 public class ControllerProduct {
+    @Autowired
+    private ProductService productService;
+
     @PostMapping
     public ResponseEntity<String> create(){
+        productService.createProduct(null);
+
         return ResponseEntity.status(201).body("Produto cadastrado");
     }
 
     @PutMapping
     public ResponseEntity<String> update(){
+        productService.updateProduct(null, null);
+
         return ResponseEntity.status(200).body("Produto atualizado");
     }
 
     @GetMapping
     public ResponseEntity<String> find(){
+        productService.getProductById(null);
+
         return ResponseEntity.status(200).body("Maça");
     }
 
     @DeleteMapping
     public ResponseEntity<Void> delete(){
+        productService.deleteProduct(null);
+
         return ResponseEntity.status(204).build();
     }
 }
