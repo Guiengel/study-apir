cd85c261de506ac108b3b24a7bfa100f9742ddd2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 21:01:45 -0300

    feat: adding Products

--- src/main/java/com/github/guiengel/study_apir/controller/ControllerProduto.java
+++ src/main/java/com/github/guiengel/study_apir/controller/ControllerProduto.java
@@ -1,4 +1,4 @@
-package com.github.guiengel.study_apir;
+package com.github.guiengel.study_apir.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 @RestController
 @RequestMapping("Produtos")
